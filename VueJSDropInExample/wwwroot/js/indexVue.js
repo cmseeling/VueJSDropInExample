1c7027ffc913f1ed8787c8d7ff2e890648670161
cmseeling <cmseeling@gmail.com>
2018-10-24 17:26:19 -0500

    init

---
+++ VueJSDropInExample/wwwroot/js/indexVue.js
@@ -0,0 +1,10 @@
+﻿new Vue({
+    el: '#vueJS',
+    data: {
+        todos: [
+            "VueJS presentation",
+            "Make dinner",
+            "Rewrite all of the front ends"
+        ]
+    }
+})
