f71af165faf69954913647b418baef611dcb1737
cmseeling <cmseeling@gmail.com>
2018-10-24 17:31:31 -0500

    added to list of things to do

--- VueJSDropInExample/wwwroot/js/indexVue.js
+++ VueJSDropInExample/wwwroot/js/indexVue.js
@@ -4,7 +4,10 @@
         todos: [
             "VueJS presentation",
             "Make dinner",
-            "Rewrite all of the front ends"
+            "Rewrite all of the front ends",
+            "change this array to an array of objects",
+            "event handling for marking item as complete",
+            "make a computed property off of which ones are completed"
         ]
     }
 })
